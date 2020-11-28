480315df95668484005a4a8bc533ea6e64bfe8b6
callum <cmccreadie1@sheffield.ac.uk>
2020-11-28 23:48:56 +0000

    Login WORKs!!!!!

--- src/Controllers/LogIn/LogInController.java
+++ src/Controllers/LogIn/LogInController.java
@@ -1,5 +1,13 @@
 package Controllers.LogIn;
 
+
+import Models.Authentication.Login;
+import Views.LogIn;
+import Views.Student.StudentWelcomeScreen;
+import com.mysql.cj.log.Log;
+
+import javax.swing.*;
+
 /**
  * Action listeners are added in login class and methods are called here
  * an instance of this is created in login
@@ -7,11 +15,24 @@ package Controllers.LogIn;
  */
 public class LogInController {
 
-    public LogInController() {
+    private LogIn loginFrame;
 
+    public LogInController() {
+        this.loginFrame = new LogIn("University Management System",this);
+        loginFrame.setVisible(true);
     }
+
     public void loginButtonPress(String userName, String password){
-        System.out.print("Implement This" + userName + password);
+        if (Login.loginAuthenticated(userName,password)){
+            // if password matched instantiate new view
+            JFrame frame = new Views.Student.StudentWelcomeScreen(new Object[]{"Placeholder","for","grades"});
+            frame.setVisible(true);
+            loginFrame.dispose();
+        }
+    }
+
+    public static void main(String[] args){
+        LogInController logInController = new LogInController();
     }
 
 }
