ce2f6f6b2d0614346282dc78201cec79b1d8e884
Bobby Williams <bwilliams4@sheffield.ac.uk>
2020-12-03 22:01:11 +0000

    Minor changes

--- src/Views/Teacher/teacherInspect.java
+++ src/Views/Teacher/teacherInspect.java
@@ -4,16 +4,10 @@ import javax.swing.*;
 
 public class teacherInspect {
     private JPanel buttonPanel;
-    private JLabel moduleCreditsLabel;
-    private JButton backButt;
-    private JLabel addModuleLabel;
-    private JComboBox optionalModulesCombo;
-    private JButton submitButt;
-    private JTextField moduleCreditsTakenTxtField;
     private JProgressBar progressBar1;
     private JTable studentModulesTable;
     private JLabel studentInfoLabel;
-    private JButton removeSelectedButton;
     private JButton progressStudentToNextButton;
     private JButton retakeLevelButton;
+    private JPanel mainPanel;
 }
