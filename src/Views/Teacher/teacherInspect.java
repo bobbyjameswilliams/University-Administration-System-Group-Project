10ceb0ae4c211743e9150989b3feebabb5315a7c
Bobby Williams <bwilliams4@sheffield.ac.uk>
2020-12-03 21:57:09 +0000

    Added teacher inspect view for student progression

---
+++ src/Views/Teacher/teacherInspect.java
@@ -0,0 +1,19 @@
+package Views.Teacher;
+
+import javax.swing.*;
+
+public class teacherInspect {
+    private JPanel buttonPanel;
+    private JLabel moduleCreditsLabel;
+    private JButton backButt;
+    private JLabel addModuleLabel;
+    private JComboBox optionalModulesCombo;
+    private JButton submitButt;
+    private JTextField moduleCreditsTakenTxtField;
+    private JProgressBar progressBar1;
+    private JTable studentModulesTable;
+    private JLabel studentInfoLabel;
+    private JButton removeSelectedButton;
+    private JButton progressStudentToNextButton;
+    private JButton retakeLevelButton;
+}
