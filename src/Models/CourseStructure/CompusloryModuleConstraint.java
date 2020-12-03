6fdd8274b78e40d4b48cda22ba779da6c9bfeb66
callum <cmccreadie1@sheffield.ac.uk>
2020-12-03 21:51:22 +0000

    I

---
+++ src/Models/CourseStructure/CompusloryModuleConstraint.java
@@ -0,0 +1,9 @@
+package Models.CourseStructure;
+
+public class CompusloryModuleConstraint extends Exception {
+
+    @Override
+    public void printStackTrace(){
+        System.out.println("Cant remove Compulsory Modules");
+    }
+}
