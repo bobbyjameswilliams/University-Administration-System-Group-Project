f74121f11d506e2c2a59950f5008a4cf72c8f459
Bobby Williams <bwilliams4@sheffield.ac.uk>
2020-12-04 12:19:11 +0000

    Fixed bug with the personal tutors table

--- src/Models/UserAccounts/Administrator.java
+++ src/Models/UserAccounts/Administrator.java
@@ -100,7 +100,10 @@ public class Administrator extends Employee {
         UserManipulator.addUser(student);
         String values = student.getStudentDetailsForInserting();
         DBController.executeCommand("INSERT INTO Student VALUES ('"+values+"');");
-        DBController.executeCommand("INSERT INTO PersonalTutor (regNumber) VALUES ("+student.getRegNumber()+");");
+        Student student1 = new UserAccountBuilder(student.getUsername()).studentBuilder();
+        //TODO: Delete below
+        String delThis = "INSERT INTO PersonalTutor (regNumber) VALUES ("+student1.getRegNumber()+");";
+        DBController.executeCommand(delThis);
     }
 
     public void removeStudent(int regNumber){
